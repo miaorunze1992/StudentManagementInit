1949465d0471eaaf48146c82772a656ee425b16c
miaorz <runze1992@163.com>
2023-03-14 15:23:12 +0900

    解决mybatis，mysql连接问题

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/StudentManagementApplication.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/StudentManagementApplication.java
@@ -2,8 +2,9 @@ package co.jp.cloudbyte.StudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
